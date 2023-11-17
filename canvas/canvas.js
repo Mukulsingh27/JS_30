347461f7cfeefa2335bb22230721efe3d07d0cc4
Mukul Singh <mukulsingh3344@gmail.com>
2023-11-17 15:47:51 +0530

    Add canvas

---
+++ canvas/canvas.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// Draw function.
+function draw( event ) {
+	// Bail.
+	if( ! isDrawing ) return;
+
+	// set the stroke color.
+	ctx.strokeStyle = `hsl(${ hue }, 100%, 50%)`;
+
+	// Draw.
+	ctx.beginPath();
+
+	// Start from.
+	ctx.moveTo( lastX, lastY );
+
+	// Go to.
+	ctx.lineTo( event.offsetX, event.offsetY );
+	ctx.stroke();
+
+	// Update the last position.
+	[ lastX, lastY ] = [ event.offsetX, event.offsetY ];
+
+	// Reset the hue.
+	if( hue >= 360 ) hue = 0;
+
+	// Update the hue.
+	hue++;
+}
+
+// Selectors.
+const canvas = document.querySelector( '#canvas' );
+const ctx = canvas.getContext( '2d' );
+
+// Initialize.
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue	= 0;
+
+// Make the canvas size equal to the window size.
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Default stroke style.
+ctx.strokeStyle = '#000';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 50;
+ctx.globalCompositeOperation = 'multiply';
+
+// Event listeners.
+canvas.addEventListener( 'mousedown', ( event ) => {
+	isDrawing = true;
+	// Update the last position.
+	[ lastX, lastY ] = [ event.offsetX, event.offsetY ];
+} );
+canvas.addEventListener( 'mousemove', draw );
+canvas.addEventListener( 'mouseup', () => isDrawing = false );
+canvas.addEventListener( 'mouseout' , () => isDrawing = false );
